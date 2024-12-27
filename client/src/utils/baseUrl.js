8ca642f5757c56f8d29442efabecd145fa5c0811
AliJamalov <168686371+AliJamalov@users.noreply.github.com>
2024-12-27 17:04:37 +0400

    url change

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const isLocal = window.location.hostname === "localhost";
+const local = isLocal
+  ? "http://localhost:5000/api"
+  : "https://rent-dress.onrender.com";
+
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: local,
 });
 
 instance.interceptors.request.use((config) => {
--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://rent-dress.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
